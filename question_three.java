a6f7d7ed7a125baf2ca4dfedc74d3df4b5ac5621
mattcee <mattcee@users.noreply.github.com>
2014-03-20 20:24:47 -0700

    Update question_three.java

--- question_three.java
+++ question_three.java
@@ -2,37 +2,45 @@ public class question_three{
 
   public static void remove(char[] word)
   {
+    //return if null
     if(word == null)
     {
       return;
     }
+    //return if the only one character in string
     if(word.length < 2)
     {
       return;
     }
+    //create a boolean array to check for repeat values
     boolean[] hit = new boolean[256];
     for(int i = 0; i< 256; i++)
     {
       hit[i] = false;
     }
+    //used for the next value after the very first value
     int tail = 1;
+    //first value always true
     hit[word[0]] = true;
+    //check for repeat values
     for(int i = 1; i<word.length; ++i)
     {
+      //if not a repeating letter, set hit to true and add it to the array
       if(!hit[word[i]])
       {
-
+        
         word[tail] = word[i];
         ++tail;
         hit[word[i]] = true;
       }
       else
       {
-
+        //remove repition
         word[i] = 0;
       }
 
     }
+    //remove the rest of the reptition
     int reduce = word.length - tail;
     for(int i = 0; i<reduce; i++)
     {
